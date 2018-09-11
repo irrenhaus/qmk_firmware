30ad3daae96667f8c6e26ee62353a941dbaf989d
Nils Hesse <n.hesse@patagona.de>
2018-09-11 18:56:29 +0200

    Moving of some function keys

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/neo2/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/neo2/keymap.c
@@ -21,13 +21,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTZ] = LAYOUT_5x6(
      KC_ESC , DE_1  , DE_2  , DE_3  , DE_4  , DE_5  ,                         DE_6  , DE_7  , DE_8  , DE_9  , DE_0  ,DE_MINS,
-     KC_TAB , DE_X  , DE_V  , DE_L  , DE_C  , DE_W  ,                         DE_K  , DE_H  , DE_G  , DE_F  , DE_Q  ,DE_SS,
+     XXXXXXX, DE_X  , DE_V  , DE_L  , DE_C  , DE_W  ,                         DE_K  , DE_H  , DE_G  , DE_F  , DE_Q  ,DE_SS,
      XXXXXXX, DE_U  , DE_I  , DE_A  , DE_E  , DE_O  ,                         DE_S  , DE_N  , DE_R  , DE_T  , DE_D  ,DE_Y,
-     XXXXXXX, DE_UE , DE_OE , DE_AE , DE_P  , DE_Z  ,                         DE_B  , DE_M  ,DE_COMM,DE_DOT , DE_J  ,DE_PLUS
+      KC_TAB, DE_UE , DE_OE , DE_AE , DE_P  , DE_Z  ,                         DE_B  , DE_M  ,DE_COMM,DE_DOT , DE_J  ,DE_PLUS
                      DE_LEFT,DE_RGHT,                                                        DE_UP  ,DE_DOWN,
-                                     KC_LSFT, KC_SPC,                         KC_ENT,KC_BSPC,
-                                     MOD3   ,KC_LGUI,                        KC_RGUI, MOD3  ,
-                                     KC_LCTL,KC_LALT,                        KC_RALT,KC_RCTL
+                                     KC_SPC, KC_LSFT,                        KC_BSPC, KC_ENT,
+                                     MOD3   ,KC_LALT,                        KC_RALT, MOD3  ,
+                                     KC_LCTL,KC_LGUI,                        KC_RGUI,KC_RCTL
   ),
 
   [_MOD3] = LAYOUT_5x6(
